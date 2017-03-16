235983d2cfbf1dd95ef00931a832d6e9ed30cc3b
Elizabeth KELEN <ekelen@e1z3r4p2.42.us.org>
2017-03-15 18:05:05 -0700

    Adds printf

---
+++ printf/srcs/ft_atoi_base.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi_base.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ekelen <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/20 16:16:15 by ekelen            #+#    #+#             */
+/*   Updated: 2017/02/20 16:16:47 by ekelen           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+static int	ft_ibase(char c, int base)
+{
+	if (base <= 10)
+		return (c >= '0' && c <= '9');
+	return ((c >= '0' && c <= '9') || (c >= 'A' && c <= ('A' + base - 10)));
+}
+
+int			ft_atoi_base(char *str, int base)
+{
+	int	value;
+
+	value = 0;
+	if (base <= 1 || base > 36)
+		return (0);
+	while (ft_ibase(*str, base))
+	{
+		if (*str - 'A' >= 0)
+			value = value * base + (*str - 'A' + 10);
+		else
+			value = value * base + (*str - '0');
+		str++;
+	}
+	return (value);
+}
