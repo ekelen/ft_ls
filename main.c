34954881984ae4999954bf1f5501dc33d088d700
Elizabeth KELEN <ekelen@e1z3r3p22.42.us.org>
2017-03-17 00:36:49 -0700

    Sloppy recursive thing

--- main.c
+++ main.c
@@ -43,46 +43,44 @@ static int	s_dir_info(t_ls *new, t_ls **tree)
 	return (1);
 }
 
-int		get_new(char *s, struct dirent *dp, t_ls **tree)
+int		get_new(struct stat stp, char *s, struct dirent *dp, t_ls **tree)
 {
-	struct stat stp;
+	
 	//char *s2 = ft_strjoin(s, "/");
-	char *s3 = ft_strjoin(s, dp->d_name);
-	printf("s3 : %s\n", s3);
+	
+	//printf("s3 : %s\n", s3);
 	//free(s2);
 	t_ls *new;
 	new = malloc(sizeof(t_ls));
-	if (stat(s3, &stp) < 0)
-	{
-		ft_printf("Stat problem\n");
-		return(1);
-	}
+	(void)stp;
+	(void)s;
+	
 
 
 	   //struct stat stp;
     // if(stat(av[1],&stp) < 0)    
     //     return 1;
  
-    printf("Information for %s\n",s);
-    printf("---------------------------\n");
-    printf("File Size: \t\t%lld bytes\n",stp.st_size);
-    printf("Number of Links: \t%d\n",stp.st_nlink);
-    printf("File inode: \t\t%lld\n",stp.st_ino);
+    // printf("Information for %s\n",s);
+    // printf("---------------------------\n");
+    // printf("File Size: \t\t%lld bytes\n",stp.st_size);
+    // printf("Number of Links: \t%d\n",stp.st_nlink);
+    // printf("File inode: \t\t%lld\n",stp.st_ino);
  
-    printf("File Permissions: \t");
-    printf( (S_ISDIR(stp.st_mode)) ? "d" : "-");
-    printf( (stp.st_mode & S_IRUSR) ? "r" : "-");
-    printf( (stp.st_mode & S_IWUSR) ? "w" : "-");
-    printf( (stp.st_mode & S_IXUSR) ? "x" : "-");
-    printf( (stp.st_mode & S_IRGRP) ? "r" : "-");
-    printf( (stp.st_mode & S_IWGRP) ? "w" : "-");
-    printf( (stp.st_mode & S_IXGRP) ? "x" : "-");
-    printf( (stp.st_mode & S_IROTH) ? "r" : "-");
-    printf( (stp.st_mode & S_IWOTH) ? "w" : "-");
-    printf( (stp.st_mode & S_IXOTH) ? "x" : "-");
-    printf("\n\n");
+    // printf("File Permissions: \t");
+    // printf( (S_ISDIR(stp.st_mode)) ? "d" : "-");
+    // printf( (stp.st_mode & S_IRUSR) ? "r" : "-");
+    // printf( (stp.st_mode & S_IWUSR) ? "w" : "-");
+    // printf( (stp.st_mode & S_IXUSR) ? "x" : "-");
+    // printf( (stp.st_mode & S_IRGRP) ? "r" : "-");
+    // printf( (stp.st_mode & S_IWGRP) ? "w" : "-");
+    // printf( (stp.st_mode & S_IXGRP) ? "x" : "-");
+    // printf( (stp.st_mode & S_IROTH) ? "r" : "-");
+    // printf( (stp.st_mode & S_IWOTH) ? "w" : "-");
+    // printf( (stp.st_mode & S_IXOTH) ? "x" : "-");
+    // printf("\n\n");
  
-    printf("The file %s a symbolic link\n", (S_ISLNK(stp.st_mode)) ? "is" : "is not");
+    // printf("The file %s a symbolic link\n", (S_ISLNK(stp.st_mode)) ? "is" : "is not");
 
 
 	ft_strcpy(new->name, dp->d_name);
@@ -94,30 +92,73 @@ int		get_new(char *s, struct dirent *dp, t_ls **tree)
 }
 
 
-
-int		main(int ac, char **av)
+void	rec_check(char *s, t_ls **tree)
 {
-	DIR *dirp;
-    struct dirent *dp;
-    t_ls 	*tree;
-    tree = NULL;
-    if (ac > 2)
-    	return (0);
-	dirp = opendir(av[1]);
-	//rec_check(av[1], dp, &tree);
-	while (dirp) 
+	DIR 			*dir;
+    struct dirent	*dp;
+    struct stat		stp;
+    // char			*s3;
+
+	dir = opendir(s);
+	while (dir) 
 	{
-	    if ((dp = readdir(dirp)) != NULL) 
+	    if ((dp = readdir(dir)) != NULL) 
 	    {
-	    	get_new(av[1], dp, &tree);	
+	    	if (stat(s, &stp) < 0)
+			{
+				// printf("problematic s3 : %s\n", s3);
+				ft_printf("Stat problem\n");
+				return ;
+			}
+			if (S_ISDIR(stp.st_mode))
+			{
+				printf("s recursive : %s\n", s);
+				rec_check((ft_strjoin(s, dp->d_name)), tree);
+			}
+		    get_new(stp, s, dp, tree);
 	    }
 	    else 
 	   	{
-	   		tree_pr(tree);
-	        closedir(dirp);
-	        return (0);
+	   		tree_pr(*tree);
+	        closedir(dir);
+	        return ;
 	    }
 	}
+
+}
+
+
+int		main(int ac, char **av)
+{
+	t_ls *tree;
+	tree = NULL;
+	if (ac != 2)
+	{
+		printf("handle multiple inputs soon....\n");
+		return (0);
+	}
+	rec_check(av[1], &tree);
+	// DIR *dirp;
+ //    struct dirent *dp;
+ //    t_ls 	*tree;
+ //    tree = NULL;
+ //    if (ac > 2)
+ //    	return (0);
+	// dirp = opendir(av[1]);
+	// //rec_check(av[1], dp, &tree);
+	// while (dirp) 
+	// {
+	//     if ((dp = readdir(dirp)) != NULL) 
+	//     {
+	//     	get_new(av[1], dp, &tree);	
+	//     }
+	//     else 
+	//    	{
+	//    		tree_pr(tree);
+	//         closedir(dirp);
+	//         return (0);
+	//     }
+	// }
 
 
 
